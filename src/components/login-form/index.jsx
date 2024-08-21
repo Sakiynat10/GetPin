ea5f0ae54d51434850f2bdde575dcdbfb5a8900c
Sakiynat10 <xoliyorvasilbek4@gmail.com>
2024-08-21 11:49:46 +0500

    chjange

--- src/components/login-form/index.jsx
+++ src/components/login-form/index.jsx
@@ -5,17 +5,29 @@ import "./style.scss"
 import {useRouter} from "next/navigation";
 
 const LoginForm = () => {
-    const router = useRouter()
-  const  handleUserValue = () => {
-      rouer.push(`/profile`)
+    const router = useRouter() ;
+
+  // const  handleUserValue = () => {
+  //     router.push(`/profile`)
+  // }
+
+  const login = (e) => {
+      e.preventDefault();
+      const elements = e.currentTarget.elements;
+      const user = {
+           parol: elements[0]?.value,
+           password: elements[0]?.value,
+      }
+      console.log(user)
   }
+
   return (
     <div className="login-content">
         <h1>Getpin</h1>
-        <form>
-            <input type="text" placeholder='Login' />
-            <input type="text" placeholder='Parol' />
-            <button onClick={handleUserValue} type='submit'>Войти</button>
+        <form onSubmit={login}>
+            <input type="text" name={"login"} placeholder='Login' />
+            <input type="password" name={"password"} placeholder='Parol' />
+            <button  type='submit'>Войти</button>
         </form>
     </div>
   )
