eb2923e3e50be720092c5b52517db5e722a69347
Sakiynat10 <xoliyorvasilbek4@gmail.com>
2024-09-09 12:07:53 +0500

    fixed

--- src/components/directions-dropdown/index.jsx
+++ src/components/directions-dropdown/index.jsx
@@ -1,34 +1,37 @@
-"use client"
+"use client";
 
-import "./style.scss"
-import {categoryData} from "@/data";
-import {useState} from "react";
+import "./style.scss";
+import { useDetectClickOutside } from "react-detect-click-outside";
+import { useState } from "react";
 
 const DirectionDropdown = () => {
-    const [drop , setDrop] = useState(false) ;
-    const [dropActive, setDropActive] = useState("Маркетинг");
-    const handleDirectionDrop = () => {
-          setDrop(!drop);
-    }
-    const closeDrop = (e) => {
-        e.preventDefault();
-        e.stopPropagation()
-        setDrop(false);
-    }
+    const [drop, setDrop] = useState(false);
 
-    return(
-        <button onClick={handleDirectionDrop} className={"direction-drop"}>
+    const handleDirectionDrop = () => {
+        setDrop(!drop);
+    };
+
+    const closeDrop = () => {
+        setDrop(false);
+    };
+
+    // Detect click outside using `react-detect-click-outside`
+    const ref = useDetectClickOutside({ onTriggered: closeDrop });
+
+    return (
+        <button ref={ref} onClick={handleDirectionDrop} className="direction-drop">
             <img src="/directions.svg" alt="direction" />
             Направления
-            <span className={drop ? "all-subcategories"  :"all-subcategories hidden-drop"}>
-                <span onClick={closeDrop} >Маркетинг</span>
-                <span onClick={closeDrop} >Образование</span>
-                <span onClick={closeDrop} >ИТ</span>
-                <span onClick={closeDrop} >Культура</span>
-                <span onClick={closeDrop} >Маркетинг</span>
-                <span onClick={closeDrop} >Бизнес</span>
-            </span>
+            <span className={drop ? "all-subcategories" : "all-subcategories hidden-drop"}>
+        <span onClick={closeDrop}>Маркетинг</span>
+        <span onClick={closeDrop}>Образование</span>
+        <span onClick={closeDrop}>ИТ</span>
+        <span onClick={closeDrop}>Культура</span>
+        <span onClick={closeDrop}>Маркетинг</span>
+        <span onClick={closeDrop}>Бизнес</span>
+      </span>
         </button>
-    )
-}
+    );
+};
+
 export default DirectionDropdown;
